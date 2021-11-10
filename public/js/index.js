751fa81e0071bab853ead9756b742d540458f233
Oladokun <oladipupooladokun@gmail.com>
2021-11-09 20:12:06 -0800

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-    let greeting = '<h1 class="slide-in-elliptic-top-fwd">hello world</h1>';
+    let greeting = '<h1 class="slide-in-elliptic-top-fwd">content goes here</h1>';
     $('body').append(greeting)
 });
