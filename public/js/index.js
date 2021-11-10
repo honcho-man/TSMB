1ca5d9fcdee4709e829c1195298e08e75853958a
Oladokun <oladipupooladokun@gmail.com>
2021-11-10 13:08:35 -0800

    offcanvas close function limited to sm screen

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@
 $('.lg-nav, .sm-nav').find('li').addClass('btn-ripple');
 $('.primary-btn').addClass('btn-ripple')
 //close side-bar onclick link
-$('.lg-nav, .sm-nav').find('li').attr('data-bs-dismiss', 'offcanvas');
+$('.sm-nav').find('li').attr('data-bs-dismiss', 'offcanvas');
 //change active classes of nav links
 $('.lg-nav, .sm-nav').find('li').on('click', function(e){
     if(!e.currentTarget.classList.contains('active-nav')) {
