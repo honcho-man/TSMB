ad96b98fae668c729a930be86049a708c1e8f00f
Oladokun <oladipupooladokun@gmail.com>
2021-11-10 12:58:56 -0800

    header, body updated

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,12 @@
-$(document).ready(function () {
-    let greeting = '<h1 class="slide-in-elliptic-top-fwd">content goes here</h1>';
-    $('body').append(greeting)
-});
+//add ripple effect to buttons
+$('.lg-nav, .sm-nav').find('li').addClass('btn-ripple');
+$('.primary-btn').addClass('btn-ripple')
+//close side-bar onclick link
+$('.lg-nav, .sm-nav').find('li').attr('data-bs-dismiss', 'offcanvas');
+//change active classes of nav links
+$('.lg-nav, .sm-nav').find('li').on('click', function(e){
+    if(!e.currentTarget.classList.contains('active-nav')) {
+        $('.lg-nav, .sm-nav').find('li.active-nav').removeClass('active-nav')
+        e.currentTarget.classList.add('active-nav')
+    }
+})
