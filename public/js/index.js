06bfc31c4d24bce85dd29bf98b4e15897919bbda
Oladokun <oladipupooladokun@gmail.com>
2021-11-12 15:27:55 -0800

    update

--- public/js/index.js
+++ public/js/index.js
@@ -10,3 +10,40 @@ $('.lg-nav, .sm-nav').find('li').on('click', function(e){
         e.currentTarget.classList.add('active-nav')
     }
 })
+//copy acct no on click
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).html().trim()).select();
+    document.execCommand("copy");
+    $temp.remove();
+    $('.copy-text').html('copied!')
+}
+$(function() {
+    // This code will attach `fileselect` event to all file inputs on the page
+    $(document).on('change', ':file', function() {
+      var input = $(this),
+          numFiles = input.get(0).files ? input.get(0).files.length : 1,
+          label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+      input.trigger('fileselect', [numFiles, label]);
+    });
+  
+    
+    $(document).ready( function() {
+  //below code executes on file input change and append name in text control
+        $(':file').on('fileselect', function(event, numFiles, label) {
+  
+            var input = $(this).parents('.input-group').find(':text'),
+                log = numFiles > 1 ? numFiles + ' files selected' : label;
+  
+            if( input.length ) {
+                input.val(log);
+            } else {
+                if( log ) alert(log);
+            }
+  
+        });
+    });
+});
+//require all input
+$('input,textarea').attr('required', true)
